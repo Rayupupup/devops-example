e801c833fc7196cefaafeb1492b27638f3ec764c
ray <liwenjun2049@gmail.com>
2022-06-28 15:19:51 +0800

    Initial commit

---
+++ src/main/java/io/ray/example/devops/DevopsExampleApplication.java
@@ -0,0 +1,13 @@
+package io.ray.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ray/example/devops/DevopsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ray.example.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
